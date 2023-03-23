9c835b1cbcec5edc0aa12e7d87969c290b185fd4
Vic8t <vic8.taillieu@gmail.com>
2023-03-23 19:36:26 +0100

    Implement home window

--- actions.py
+++ actions.py
@@ -1,4 +1,49 @@
+import PySimpleGUI as sg
+from PIL import Image, ImageTk
+
 from utils import fen_to_png
+
+
+def main_layout():
+    layout = [
+        [sg.Text(key="-TITLE-", font=("Helvetica", 20))],
+        [sg.Image(key="-IMAGE-")],
+
+        [sg.Button("Test rapide", key="-SHORT-")],
+        [sg.Button("Test complet", key="-LONG-")],
+        [sg.Button("Reprendre", key="-LOAD-")]
+
+    ]
+
+    window = sg.Window("Echecs : le test", layout, finalize=True, element_justification="center")
+    window["-IMAGE-"].update(data=ImageTk.PhotoImage(Image.open("book_cover.jpg")))
+
+    return window
+
+
+def quiz_layout():
+    layout = [
+        [sg.Text(key="-QUESTION-", font=("Helvetica", 20))],
+        [sg.Image(key="-IMAGE-")],
+
+        [sg.Text(key="-QUESTION1-", font=("Helvetica", 20))],
+
+        [sg.Radio("A", "Q1", key="-A1-", font=("Helvetica", 12))],
+        [sg.Radio("B", "Q1", key="-B1-", font=("Helvetica", 12))],
+        [sg.Radio("C", "Q1", key="-C1-", font=("Helvetica", 12))],
+        [sg.Radio("D", "Q1", key="-D1-", font=("Helvetica", 12))],
+
+        [sg.Text(key="-QUESTION2-", font=("Helvetica", 20))],
+
+        [sg.Radio("A", "Q2", key="-A2-", font=("Helvetica", 12))],
+        [sg.Radio("B", "Q2", key="-B2-", font=("Helvetica", 12))],
+        [sg.Radio("C", "Q2", key="-C2-", font=("Helvetica", 12))],
+        [sg.Radio("D", "Q2", key="-D2-", font=("Helvetica", 12))],
+
+        [sg.Button("Valider", key="-VALIDER-")]
+    ]
+
+    return sg.Window("Echecs : le test", layout, finalize=True)
 
 
 def next_position(window, data, position):
--- chess_test.py
+++ chess_test.py
@@ -1,43 +1,38 @@
 import PySimpleGUI as sg
 
-from actions import next_position
+from actions import main_layout, next_position, quiz_layout
 from scores import get_position_score
 from utils import load_data
 
 # Load questions data
 data = load_data()
 
-# Layout
-layout = [
-    [sg.Text(key="-QUESTION-", font=("Helvetica", 20))],
-    [sg.Image(key="-IMAGE-")],
-
-    [sg.Text(key="-QUESTION1-", font=("Helvetica", 20))],
-
-    [sg.Radio("A", "Q1", key="-A1-", font=("Helvetica", 12))],
-    [sg.Radio("B", "Q1", key="-B1-", font=("Helvetica", 12))],
-    [sg.Radio("C", "Q1", key="-C1-", font=("Helvetica", 12))],
-    [sg.Radio("D", "Q1", key="-D1-", font=("Helvetica", 12))],
-
-    [sg.Text(key="-QUESTION2-", font=("Helvetica", 20))],
-
-    [sg.Radio("A", "Q2", key="-A2-", font=("Helvetica", 12))],
-    [sg.Radio("B", "Q2", key="-B2-", font=("Helvetica", 12))],
-    [sg.Radio("C", "Q2", key="-C2-", font=("Helvetica", 12))],
-    [sg.Radio("D", "Q2", key="-D2-", font=("Helvetica", 12))],
-
-    [sg.Button("Valider", key="-VALIDER-")]
-]
-
-window = sg.Window("Echecs : le test", layout, finalize=True)
-
-position = 0
-score = []
-next_position(window, data, position)
+# Display home window
+home = main_layout()
 
 # Run the event loop
 while True:
-    event, values = window.read()
+    event, values = home.read()
+    if event == "Exit" or event == sg.WIN_CLOSED:
+        break
+    elif event == "-SHORT-":
+        break
+    elif event == "-LONG-":
+        break
+    elif event == "-LOAD-":
+        break
+
+home.close()
+
+quiz = quiz_layout()
+
+position = 0
+score = []
+next_position(quiz, data, position)
+
+# Run the event loop
+while True:
+    event, values = quiz.read()
     if event == "Exit" or event == sg.WIN_CLOSED:
         break
     elif event == "-VALIDER-":
@@ -49,6 +44,6 @@ while True:
         if position == len(data):
             sg.popup(f"Votre score est de {score} points.")
         else:
-            next_position(window, data, position)
+            next_position(quiz, data, position)
 
-window.close()
+quiz.close()
